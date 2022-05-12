e8773bdf540267b30904418a8f3002334352f5af
Evan Dartez <98479231+esdartez@users.noreply.github.com>
2022-05-11 22:17:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,21 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
+
+    dist = {node: -1 for node in graph}
+    weight = {node: -1 for node in graph}
+
+    dist[source] = 0
+    weight[source] = 0
+    heap = [(0, source)]  
+    while heap:
+        d, top = heappop(heap)  
+        for node, w in graph[top]:  
+            if dist[node] == -1 or dist[node] > d + 1:
+                dist[node] = d + 1
+                weight[node] = weight[top] + w
+                heappush(heap, (d + 1, node))
+    return {node: (dist[node], weight[node]) for node in graph}
     
 def test_shortest_shortest_path():
 
@@ -40,8 +53,17 @@ def bfs_path(graph, source):
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+que = deque()
+    que.append(source)
+    parent = {source: None}
+    while que:
+        leftmost = que.popleft()
+        for node in graph[leftmost]:
+            if node not in parent:  
+                parent[node] = leftmost 
+                que.append(node)
+    del parent[source]  
+    return parent
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -65,8 +87,11 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    path = []
+    while destination in parents:
+        path.append(parents[destination])
+        destination = parents[destination]
+    return "".join(path[::-1])
 
 def test_get_path():
     graph = get_sample_graph()
